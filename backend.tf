8d48ef31c9232ac8fd1818e0d439d5913ef3e705
Mudit Mathur <85672841+mudit097@users.noreply.github.com>
2024-01-13 13:58:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "bucket97"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "mudit_Db"
   }
 }
